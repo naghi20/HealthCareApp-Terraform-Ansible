cc2d04b5ac39ab9b09f7e7bfc491ac7e890d17e2
Ahmed AL Naghi <64667680+naghi20@users.noreply.github.com>
2026-07-13 22:35:37 +0300

    Add outputs for bastion IP, ALB DNS, and DB endpoint

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,12 @@
+output "bastion_public_ip" {
+  value = aws_instance.bastion.public_ip
+}
 
+output "alb_dns_name" {
+  value = aws_lb.app.dns_name
+}
+
+output "db_endpoint" {
+  value     = aws_db_instance.app_db.endpoint
+  sensitive = true
+}
