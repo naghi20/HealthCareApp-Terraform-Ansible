1ac8642453d19c17472bb1cae5c68eab372a9033
Ahmed AL Naghi <64667680+naghi20@users.noreply.github.com>
2026-07-13 22:33:16 +0300

    Add IAM role and instance profile for app

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,1 +1,21 @@
+resource "aws_iam_role" "app_role" {
+  name = "lab-app-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "://amazonaws.com" }
+    }]
+  })
+}
 
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.app_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "app_profile" {
+  name = "lab-app-instance-profile"
+  role = aws_iam_role.app_role.name
+}
