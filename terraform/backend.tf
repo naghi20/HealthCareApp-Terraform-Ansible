342a95354f8ed536478a575a22377e83d1f88087
Ahmed AL Naghi <64667680+naghi20@users.noreply.github.com>
2026-07-13 22:31:38 +0300

    Configure Terraform backend with S3 and DynamoDB

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,1 +1,20 @@
+terraform {
+  required_version = ">= 1.7.0"
+  backend "s3" {
+    bucket         = "adi-lab-tfstate-2026"
+    key            = "3tier-lab/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = var.aws_region
+}
